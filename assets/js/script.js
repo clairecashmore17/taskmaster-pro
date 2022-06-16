dda874aff00edbcc4f729eb5d1b1aa862d9fa333
Claire Cashmore <clairecashmore2017@gmail.com>
2022-06-16 15:08:59 -0400

    added due dates and button color warning

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,9 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
+
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -139,21 +142,29 @@ $(".list-group").on("click", "span", function(){
   .trim();
 
   //create new input element
-  var dataInput = $("<input>")
+  var dateInput = $("<input>")
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
 
     //swap out elements
-    $(this).replaceWith(dataInput);
+    $(this).replaceWith(dateInput);
 
+    //enable jquery ui datepicker
+    dateInput.datepicker({
+      minDate: 1,
+      onClose: function() {
+        //when calnedar is closed,, force a "change" event on the 'dateInput'
+        $(this).trigger("change");
+      }
+    });
     // automaticall focus in on new element
-    dataInput.trigger("focus");
+    dateInput.trigger("focus");
 });
 
 //value of due date was change, blur it
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -181,6 +192,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 $(".card .list-group").sortable({
   connectWith: $(".card .list-group"),
@@ -228,7 +242,9 @@ $(".card .list-group").sortable({
     console.log(tempArr);
   }
 });
-
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
 //Drag Trash enhancement
 $("#trash").droppable({
   accept: ".card .list-group-item",
@@ -244,6 +260,29 @@ $("#trash").droppable({
     console.log("out");
   }
 });
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  //ensure it worked
+  console.log(date);
+
+  //convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  //remove any old classes form element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <=2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+  
+};
+
 // load tasks for the first time
 loadTasks();
 
